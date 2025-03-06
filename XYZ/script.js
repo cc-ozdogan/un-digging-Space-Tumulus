bcc1c3c7de3e986eb3c2c439bcb1e17bae665b4a
El-Pozo <rosswellstein@gmail.com>
2025-03-06 20:48:30 +0100

    Add files via upload

--- XYZ/script.js
+++ XYZ/script.js
@@ -9,8 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const stackContainerMida = document.querySelector('.stack-container-mida');
     const stackContainerClava = document.querySelector('.stack-container-clava');
     const spans = document.querySelectorAll('.clavamage1');
-    const container = document.querySelector('.stack-container-cairn, .stack-container-clava, .stack-container-topbar');
-    const stackContainerTopbar = document.querySelector('.stack-container-topbar');
+    const container = document.querySelector('.stack-container-cairn, .stack-container-clava, .stack-container-topbar, .stack-container-topbar-gob');
+    const stackContainerTopbar = document.querySelector('.stack-container-topbar, .stack-container-topbar-gob');
     
     
     let clickCount = 0;
